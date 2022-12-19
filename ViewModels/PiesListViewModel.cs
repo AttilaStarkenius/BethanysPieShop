fc1839ab4ae13193da5412d3b249fb152356483a
Attila Starkenius <symphtom@hotmail.com>
2022-12-19 23:40:30 +0100

    19.12.2022. Styling views in the solution BethanysPieShop

---
+++ ViewModels/PiesListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BethanysPieShop.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
